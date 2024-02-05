071c2c4f83017a30e62739a226dbae9c5013ec6c
aabhi02 <abhishek.a2080@gmail.com>
2024-02-05 19:26:29 +0530

    added yield feature

--- botG.py
+++ botG.py
@@ -1,6 +1,7 @@
 from langchain.llms.google_palm import GooglePalm
 from langchain.agents import AgentType, initialize_agent, load_tools
 from apis import SERPAPI_API_KEY, GOOGLE_API_KEY
+import time
 import os
 
 os.environ['SERPAPI_API_KEY'] = SERPAPI_API_KEY
@@ -11,4 +12,7 @@ tools = load_tools(['serpapi'], llm=llm)
 agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
 def query(question):
-    return agent.run(question)
+    response = agent.run(question)
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.08)
